2608f72759895b231942d7d6bd552181a8b4b07b
danhan52 <danny.hanson52@gmail.com>
2016-05-02 20:31:13 -0500

    Add papa's suggestion to have two displaying graphs.

--- server.R
+++ server.R
@@ -11,9 +11,7 @@ shinyServer(function(input, output, session) {
   
   colSch <- reactiveValues(pc = "rainbow")
 
-  output$fraction <- renderPlot({
-    plot.new()
-    plot.window(xlim = c(0, 0.5), ylim = c(0, 1), xaxs = "i", yaxs = "i")
+  output$eFrac <- renderPlot({
     top <- input$num * input$mult
     bot <- input$den * input$mult
     if (colSch$pc == "rainbow") {
@@ -24,22 +22,30 @@ shinyServer(function(input, output, session) {
         mycols <- c(mycols, "green")
       }
     }
-    pcols <- rep(mycols[1:input$num], each = input$mult)
-    
-    if (top <= bot) {
-      rect(0, seq(0, 1-1/bot, length.out = bot), 0.5,
-           seq(1/bot, 1, length.out = bot),
-           col = c(pcols, rep("white", bot - top)))
-    }
-  })
-  
-  output$numba <- renderText({
-    if (input$mult == 1) {
-      paste0(input$num, "/", input$den)
+    if (input$num > 0) {
+      pcols1 <- mycols[1:input$num]
+      pcols2 <- rep(pcols1, each = input$mult)
     } else {
-      top <- input$num * input$mult
-      bot <- input$den * input$mult
-      paste0(top, "/", bot, " = ", input$num, "/", input$den)
+      pcols1 <- NULL
+      pcols2 <- NULL
+    }
+    
+    
+    plot.new()
+    plot.window(xlim = c(0, 1.5), ylim = c(0, 1.5), xaxs = "i", yaxs = "i")
+    if (top <= bot) {
+      rect(0, seq(0, 1-1/input$den, length.out = input$den), 0.7,
+           seq(1/input$den, 1, length.out = input$den),
+           col = c(pcols1, rep("white", input$den - input$num)))
+#       if (input$mult == 1) {
+#         text(0.35, 1.25, paste0(input$num, "/", input$den), cex = 4)
+#       } else if (input$mult > 1) {
+        rect(0.8, seq(0, 1-1/bot, length.out = bot), 1.5,
+             seq(1/bot, 1, length.out = bot),
+             col = c(pcols2, rep("white", bot - top)))
+        text(0.75, 1.25, paste0(input$num, "/", input$den, " = ", top, "/", bot),
+             cex = 4)
+      # }
     }
   })
   
--- ui.R
+++ ui.R
@@ -11,13 +11,13 @@ shinyUI(fluidPage(
   includeCSS("www/stylinSoTight.css"),
   
   # Application title
-  titlePanel("Fraction demonstration"),
+  titlePanel("Learn about fractions!"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
       sliderInput("num", "Numerator:", ticks = F,
-                  min = 1, max = 15, value = 4),
+                  min = 0, max = 15, value = 4),
       sliderInput("den", "Denominator:",
                   min = 1, max = 15, value = 5),
       sliderInput("mult", "Multiplication factor", min = 1, max = 5, value = 1),
@@ -26,8 +26,7 @@ shinyUI(fluidPage(
 
     # Show a plot of the generated distribution
     mainPanel(
-      textOutput("numba"),
-      plotOutput("fraction", width = "200px", height = "400px")
+      plotOutput("eFrac", width = "400px", height = "500px")
     )
   )
 ))
